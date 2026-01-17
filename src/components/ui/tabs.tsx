09c8cbf495327a8855b9b0ff267b2c8db4d4f889
Robert Kurcina (KITROK) <kitrok@gmail.com>
2026-01-17 17:25:28 +0000

    The accordions on the Test tab maintain state, so thank you!
    However,

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -43,10 +43,8 @@ const TabsContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
-    forceMount
     className={cn(
       "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      "data-[state=inactive]:hidden",
       className
     )}
     {...props}
